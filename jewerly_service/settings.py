271b379bd696864f1b09c5684c001f82491dcf86
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-07-02 17:04:48 +0300

    Add docker-compose

--- jewerly_service/settings.py
+++ jewerly_service/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'jewerly_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("DATABASE_ENGINE"),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
